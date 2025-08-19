7fcbe70b7b80a86b93d001bd3638b5dcde9a7b43
Guilherme Viana <imluizguilara@hotmail.com>
2025-08-19 18:34:49 -0300

    style(monorepo): format code

--- packages/prettier-config/index.js
+++ packages/prettier-config/index.js
@@ -1,12 +1,10 @@
-import { fileURLToPath } from 'node:url'
-
 /** @typedef {import("prettier").Config} PrettierConfig */
 /** @typedef {import("prettier-plugin-tailwindcss").PluginOptions} TailwindConfig */
 
 /** @type {PrettierConfig | TailwindConfig} */
 const config = {
   plugins: ['prettier-plugin-tailwindcss', 'prettier-plugin-classnames', 'prettier-plugin-merge'],
-  tailwindConfig: fileURLToPath(new URL('../tailwind-config/styles/global.css', import.meta.url)),
+  tailwindStylesheet: '../tailwind-config/src/styles/global.css',
   tailwindFunctions: ['clsx', 'cva'],
   tailwindAttributes: ['clsx', 'cva'],
   customFunctions: ['clsx', 'cva'],
@@ -15,16 +13,6 @@ const config = {
   jsxSingleQuote: true,
   printWidth: 120,
   endingPosition: 'absolute',
-  importOrder: [
-    '<TYPES>',
-    '^(react/(.*)$)|^(react$)|^(react-native(.*)$)',
-    '^(next/(.*)$)|^(next$)',
-    '<THIRD_PARTY_MODULES>',
-    '^@valley/(.*)$',
-    '^[./]',
-  ],
-  importOrderParserPlugins: ['typescript', 'jsx', 'decorators-legacy'],
-  importOrderTypeScriptVersion: '4.4.0',
   overrides: [
     {
       files: '*.json.hbs',
